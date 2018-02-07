0f79231c1dcb6b59fbc935d0ef92385208bd9cf3
Donald Sharp <sharpd@cumulusnetworks.com>
2018-02-07 15:38:00 -0500

    pimd: Compilation fixes
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/mtracebis_netlink.c
+++ pimd/mtracebis_netlink.c
@@ -132,7 +132,7 @@ int rtnl_send_check(struct rtnl_handle *rth, const char *buf, int len)
 		return -1;
 	}
 
-	for (h = (struct nlmsghdr *)resp; NLMSG_OK(h, status);
+	for (h = (struct nlmsghdr *)resp; NLMSG_OK(h, (uint32_t)status);
 	     h = NLMSG_NEXT(h, status)) {
 		if (h->nlmsg_type == NLMSG_ERROR) {
 			struct nlmsgerr *err = (struct nlmsgerr*)NLMSG_DATA(h);
@@ -214,7 +214,7 @@ int rtnl_dump_filter_l(struct rtnl_handle *rth,
 			struct nlmsghdr *h = (struct nlmsghdr*)buf;
 			msglen = status;
 
-			while (NLMSG_OK(h, msglen)) {
+			while (NLMSG_OK(h, (uint32_t)msglen)) {
 				int err;
 
 				if (nladdr.nl_pid != 0 ||
--- pimd/mtracebis_routeget.c
+++ pimd/mtracebis_routeget.c
@@ -34,14 +34,6 @@
 
 #include "mtracebis_routeget.h"
 
-static inline int rtm_get_table(struct rtmsg *r, struct rtattr **tb)
-{
-	__u32 table = r->rtm_table;
-	if (tb[RTA_TABLE])
-		table = *(__u32*) RTA_DATA(tb[RTA_TABLE]);
-	return table;
-}
-
 static int find_dst(struct nlmsghdr *n, struct in_addr *src, struct in_addr *gw )
 {
 	struct rtmsg *r = NLMSG_DATA(n);
